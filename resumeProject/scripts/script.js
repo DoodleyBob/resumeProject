588868e015ebfeb4a9b2fd40a21ed6356be140df
ManI'mDead <cte-elijah.snyder@sllboces.org>
2025-04-30 17:47:00 +0000

    slideToggle

---
+++ resumeProject/scripts/script.js
@@ -0,0 +1,29 @@
+$('#ec-desc1').css('display', 'none');
+$('#ecs1').click(function(){
+    $('#ec-desc1').slideToggle('slow');
+});
+
+$('#ec-desc2').css('display', 'none');
+$('#ecs2').click(function(){
+    $('#ec-desc2').slideToggle('slow');
+});
+
+$('#ec-desc3').css('display', 'none');
+$('#ecs3').click(function(){
+    $('#ec-desc3').slideToggle('slow');
+});
+
+$('#ec-desc4').css('display', 'none');
+$('#ecs4').click(function(){
+    $('#ec-desc4').slideToggle('slow');
+});
+
+$('#ec-desc5').css('display', 'none');
+$('#ecs5').click(function(){
+    $('#ec-desc5').slideToggle('slow');
+});
+
+$('#ec-desc6').css('display', 'none');
+$('#ecs6').click(function(){
+    $('#ec-desc6').slideToggle('slow');
+});
